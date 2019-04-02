9639734806a12272f2353ce294a12d22a5ac0eea
sos1819drp <diegoruizper@gmail.com>
2019-04-02 14:40:14 +0000

    k

--- gasAPI/index.js
+++ gasAPI/index.js
@@ -3,5 +3,8 @@ var v1 = require("./v1");
 module.exports = {
     gasIncreases : function(app, BASE_PATH){
         v1.gasIncreases(app, BASE_PATH+"/v1");
+    },
+    gasStations : function(app, BASE_PATH){
+        v1.gasStations(app, BASE_PATH+"/v1");
     }
 }
--- gasAPI/v1/index.js
+++ gasAPI/v1/index.js
@@ -1,7 +1,11 @@
 var gasIncreases = require("./gas-increases/gasIncreases.js");
+var gasStations = require("./gas-stations/gasStations.js");
 
 module.exports = {
     gasIncreases : function(app, BASE_PATH){
         gasIncreases(app, BASE_PATH);
+    },
+    gasStations : function(app, BASE_PATH){
+        gasStations(app, BASE_PATH);
     }
 }
